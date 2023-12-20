f9a5865a4464d5934f284f72d09b838bd641b36f
Ashutosh2201 <154448537+Ashutosh2201@users.noreply.github.com>
2023-12-21 02:17:52 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,8 @@ import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="footer-container">
-      2023 AP Electronics All rights reserved
+       <b> 2023 AP Electronics All rights reserved</b>
+      <div>Developed and Designed by Ashutosh Tiwari</div>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
